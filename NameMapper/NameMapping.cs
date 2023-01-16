355e0301e83bc04227ec629a46b7a01d66ac9acc
Paz Sonagara <pazsonagara@gmail.com>
2023-01-16 14:04:12 +0000

    Combinvation letter test completed for name

---
+++ NameMapper/NameMapping.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace NameMapper
+{
+	public class NameMapping
+	{
+		Dictionary<string, int> combinationLetters = new Dictionary<string, int>()
+		{
+			{"ah",5},
+            {"ch",8},
+            {"wh",16},
+            {"tz",18},
+            {"sh",21},
+            {"ta",22},
+            {"th",22},
+        };
+
+		public string ConvertCombinationLetter(string name)
+		{
+            for (int i = 0; i < name.Length - 1; i++)
+            {
+                string nameLetters = name[i] + "" + name[i + 1];
+                nameLetters = nameLetters.ToLower();
+                if (combinationLetters.ContainsKey(nameLetters))
+                {
+                    name = name.Replace(nameLetters, combinationLetters[nameLetters].ToString());
+                }
+            }
+            Console.WriteLine("Hello world " + name);
+            return name;
+
+        }
+    }
+}
+
--- NameMapperTests/NameMapperTests.cs
+++ NameMapperTests/NameMapperTests.cs
@@ -1,15 +1,22 @@
+using NUnit.Framework;
+using NameMapper;
+using FluentAssertions;
+
 namespace NameMapperTests;
 
 public class Tests
 {
+    NameMapping mapper;
+
     [SetUp]
     public void Setup()
     {
+        mapper = new NameMapping();
     }
 
     [Test]
-    public void Test1()
+    public void Should_Convert_Combination_Letter_To_Corresponding_Numerical_Value()
     {
-        Assert.Pass();
+        mapper.ConvertCombinationLetter("micheal").Should().Be("mi8eal");
     }
 }
