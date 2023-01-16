9bcab3d678fcfbfa007c05abbce92fd407b55685
Paz Sonagara <pazsonagara@gmail.com>
2023-01-16 11:29:29 +0000

    Initial project setup files

---
+++ NameMapperTests/NameMapperTests.cs
@@ -0,0 +1,15 @@
+namespace NameMapperTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
