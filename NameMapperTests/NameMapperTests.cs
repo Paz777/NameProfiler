c0a42c6b121377b7f24fff2d61586127ee218ddd
Paz Sonagara <pazsonagara@gmail.com>
2023-01-17 14:08:09 +0000

    Added textcases for all combination letters

--- NameMapperTests/NameMapperTests.cs
+++ NameMapperTests/NameMapperTests.cs
@@ -14,15 +14,21 @@ public class Tests
         mapper = new NameMapping();
     }
 
-    [TestCase("micheal", "mi8eal")]
-    [TestCase("michealtz", "mi8eal18")]
+    [TestCase("abbah", "abb5", TestName = "Should_Convert_Combination_Letter_AB_To_Corresponding_Numerical_Value")]
+    [TestCase("micheal", "mi8eal", TestName = "Should_Convert_Combination_Letter_MI_To_Corresponding_Numerical_Value")]
+    [TestCase("whitney", "16itney", TestName = "Should_Convert_Combination_Letter_WH_To_Corresponding_Numerical_Value")]
+    [TestCase("tzara", "18ara", TestName = "Should_Convert_Combination_Letter_TZ_To_Corresponding_Numerical_Value")]
+    [TestCase("shanaya", "21anaya", TestName = "Should_Convert_Combination_Letter_SH_To_Corresponding_Numerical_Value")]
+    [TestCase("talar", "22lar", TestName = "Should_Convert_Combination_Letter_TA_To_Corresponding_Numerical_Value")]
+    [TestCase("annabeth", "annabe22", TestName = "Should_Convert_Combination_Letter_AN_To_Corresponding_Numerical_Value")]
+    [TestCase("michealtz", "mi8eal18", TestName = "Should_Convert_Combination_Letter_MI_And_TZ_To_Corresponding_Numerical_Value")]
     public void Should_Convert_Combination_Letter_To_Corresponding_Numerical_Value(string name, string nameConverted)
     {
         mapper.ConvertCombinationLetter(name).Should().Be(nameConverted);
     }
 
     [TestCase("Micheal", "mi8eal")]
-    [TestCase("MicHeal", "mi8eal")]
+    [TestCase("MicHeAl", "mi8eal")]
     public void Should_Convert_Combination_Letter_To_Corresponding_Numerical_Value_With_Name_Using_Capital_Letters(string name, string nameConverted)
     {
         mapper.ConvertCombinationLetter(name).Should().Be(nameConverted);
